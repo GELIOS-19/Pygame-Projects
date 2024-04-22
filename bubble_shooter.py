258bc8fc615dd10361d82b76fc1f9b9dc1096096
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-04-22 11:30:01 -0400

    Update bubble_shooter.py

--- bubble_shooter.py
+++ bubble_shooter.py
@@ -34,62 +34,25 @@ BLACK = (0, 0, 0)
 BROWN = (94, 51, 51)
 
 
-def print_to_display(
-    display,
-    text,
-    font_name,
-    font_size,
-    bold,
-    italic,
-    color,
-    posx,
-    posy,
-    center_around_point,
-    set_custom_dimensions,
-    custom_dimension_width,
-    custom_dimension_height,
-):
-    text_parameters = pygame.font.SysFont(
-        font_name, font_size, bold=bold, italic=italic
-    )
+def print_to_display(display, text, font_name, font_size, bold, italic, color, posx, posy, center_around_point, set_custom_dimensions, custom_dimension_width, custom_dimension_height):
+    text_parameters = pygame.font.SysFont(font_name, font_size, bold=bold, italic=italic)
     rendered_text = text_parameters.render(text, True, color)
     text_dimensions = rendered_text.get_rect()
     if center_around_point == True and set_custom_dimensions == False:
         text_dimensions.center = ((posx), (posy))
         display.blit(rendered_text, text_dimensions)
     elif center_around_point == False and set_custom_dimensions == False:
-        text_dimensions.center = (
-            (posx + text_dimensions[2] / 2),
-            (posy + text_dimensions[3] / 2),
-        )
+        text_dimensions.center = ((posx + text_dimensions[2] / 2), (posy + text_dimensions[3] / 2))
         display.blit(rendered_text, text_dimensions)
     elif center_around_point == False and set_custom_dimensions == True:
-        text_dimensions.center = (
-            (posx + custom_dimension_width / 2),
-            (posy + custom_dimension_height / 2),
-        )
+        text_dimensions.center = ((posx + custom_dimension_width / 2), (posy + custom_dimension_height / 2))
         display.blit(rendered_text, text_dimensions)
     else:
-        print(
-            "error, can not set custom text dimensions when center around point is True"
-        )
+        print("error, can not set custom text dimensions when center around point is True")
 
 
 class Button(object):
-    def __init__(
-        self,
-        label,
-        label_size,
-        bold,
-        italic,
-        label_color,
-        button_x,
-        button_y,
-        button_width,
-        button_height,
-        inactive_color,
-        active_color,
-    ):
+    def __init__(self, label, label_size, bold, italic, label_color, button_x, button_y, button_width, button_height, inactive_color, active_color):
         self.label = label
         self.button_x = button_x
         self.button_y = button_y
@@ -106,32 +69,10 @@ class Button(object):
         self.mouse_click = pygame.mouse.get_pressed()
         self.mouse_position = pygame.mouse.get_pos()
 
-        if (self.button_x + self.button_width) > self.mouse_position[0] > (
-            self.button_x
-        ) and (self.button_y + self.button_height) > self.mouse_position[1] > (
-            self.button_y
-        ):
-            pygame.draw.rect(
-                win,
-                self.active_color,
-                (
-                    self.button_x,
-                    self.button_y,
-                    self.button_width,
-                    self.button_height,
-                ),
-            )
+        if (self.button_x + self.button_width) > self.mouse_position[0] > (self.button_x) and (self.button_y + self.button_height) > self.mouse_position[1] > (self.button_y):
+            pygame.draw.rect(win, self.active_color, (self.button_x, self.button_y, self.button_width, self.button_height))
         else:
-            pygame.draw.rect(
-                win,
-                self.inactive_color,
-                (
-                    self.button_x,
-                    self.button_y,
-                    self.button_width,
-                    self.button_height,
-                ),
-            )
+            pygame.draw.rect(win, self.inactive_color, (self.button_x, self.button_y, self.button_width, self.button_height))
 
         print_to_display(
             win,
@@ -150,15 +91,7 @@ class Button(object):
         )
 
     def is_clicked(self):
-        if (
-            self.button_x + self.button_width
-            > self.mouse_position[0]
-            > self.button_x
-            and self.button_y + self.button_height
-            > self.mouse_position[1]
-            > self.button_y
-            and self.mouse_click[0] == 1
-        ):
+        if self.button_x + self.button_width > self.mouse_position[0] > self.button_x and self.button_y + self.button_height > self.mouse_position[1] > self.button_y and self.mouse_click[0] == 1:
             return True
 
 
@@ -173,28 +106,16 @@ class Bubble(object):
         self.x = 500 * math.cos(theta) + 400
         self.y = 500 * math.sin(theta) + 300
         self.offset = 2000
-        self.hitbox = (
-            self.x - self.radius,
-            self.y - self.radius,
-            self.radius * 2,
-            self.radius * 2,
-        )
+        self.hitbox = (self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
         self.eff_deg = random.randint(1, 360)
 
     def draw(self, win):
-        pygame.draw.circle(
-            win, self.color, (int(self.x), int(self.y)), self.radius, 0
-        )
+        pygame.draw.circle(win, self.color, (int(self.x), int(self.y)), self.radius, 0)
 
         pygame.draw.rect(win, RED, self.hitbox, 2)
 
     def move(self):
-        self.hitbox = (
-            self.x - self.radius,
-            self.y - self.radius,
-            self.radius * 2,
-            self.radius * 2,
-        )
+        self.hitbox = (self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
         angles = math.atan2(300 - self.y, 400 - self.x)
         vel = random.randint(3, 13) / 100
         distance = math.hypot(400 - self.x, 300 - self.y)
@@ -220,12 +141,7 @@ class Effect(object):
         self.eff_color = eff_color
 
     def bubble_pop_effect(self, win):
-        pygame.draw.circle(
-            win,
-            self.eff_color,
-            (int(self.eff_x), int(self.eff_y)),
-            self.eff_radius,
-        )
+        pygame.draw.circle(win, self.eff_color, (int(self.eff_x), int(self.eff_y)), self.eff_radius)
 
         # Movement
         self.move_x = math.cos(self.eff_dir) * self.eff_vel
@@ -235,12 +151,7 @@ class Effect(object):
         self.eff_y += self.move_y
 
         # Hitbox
-        self.eff_hitbox = (
-            self.eff_x - self.eff_radius,
-            self.eff_y - self.eff_radius,
-            self.eff_radius * 2,
-            self.eff_radius * 2,
-        )
+        self.eff_hitbox = (self.eff_x - self.eff_radius, self.eff_y - self.eff_radius, self.eff_radius * 2, self.eff_radius * 2)
 
         pygame.draw.rect(win, RED, self.eff_hitbox, 2)
 
@@ -255,48 +166,26 @@ class Player:
         self.player_border_color = DARK_BLUE_GRAY
         self.point_x = 0
         self.point_y = 0
-        self.hitbox_player = (
-            self.player_x,
-            self.player_y,
-            self.width,
-            self.height,
-        )
+        self.hitbox_player = (self.player_x, self.player_y, self.width, self.height)
         self.hitbox_gun_start = (400, 300)
         self.hitbox_gun_end = (self.point_x, self.point_y)
 
     def draw(self, win):
-        pygame.draw.rect(
-            win,
-            self.player_color,
-            (self.player_x, self.player_y, self.width, self.height),
-        )
-        pygame.draw.rect(
-            win,
-            self.player_border_color,
-            (self.player_x, self.player_y, self.width, self.height),
-            5,
-        )
+        pygame.draw.rect(win, self.player_color, (self.player_x, self.player_y, self.width, self.height))
+        pygame.draw.rect(win, self.player_border_color, (self.player_x, self.player_y, self.width, self.height), 5)
 
         mouse_x, mouse_y = pygame.mouse.get_pos()
         theta = math.atan2(mouse_y - 300, mouse_x - 400)
         self.point_x = 100 * math.cos(theta) + 400
         self.point_y = 100 * math.sin(theta) + 300
 
-        pygame.draw.line(
-            win,
-            self.player_color,
-            (400, 300),
-            (self.point_x, self.point_y),
-            10,
-        )
+        pygame.draw.line(win, self.player_color, (400, 300), (self.point_x, self.point_y), 10)
 
         self.hitbox_gun_start = (400, 300)
         self.hitbox_gun_end = (self.point_x, self.point_y)
 
         pygame.draw.rect(win, RED, self.hitbox_player, 2)
-        pygame.draw.line(
-            win, RED, self.hitbox_gun_start, self.hitbox_gun_end, 2
-        )
+        pygame.draw.line(win, RED, self.hitbox_gun_start, self.hitbox_gun_end, 2)
 
 
 class Projectile(object):
@@ -314,9 +203,7 @@ class Projectile(object):
         self.y = self.point_y
 
     def draw(self, win):
-        pygame.draw.circle(
-            win, self.color, (int(self.x), int(self.y)), self.radius
-        )
+        pygame.draw.circle(win, self.color, (int(self.x), int(self.y)), self.radius)
         pygame.draw.circle(win, WHITE, (int(self.x + 4), int(self.y - 3)), 3)
 
     def move(self):
@@ -332,12 +219,8 @@ menu = True
 game = False
 settings = False
 
-start_button = Button(
-    "Start", 40, False, True, BLACK, 100, 270, 150, 150, GREEN, DARK_GREEN
-)
-quit_button = Button(
-    "Quit", 40, False, True, BLACK, 525, 270, 150, 150, RED, DARK_RED
-)
+start_button = Button("Start", 40, False, True, BLACK, 100, 270, 150, 150, GREEN, DARK_GREEN)
+quit_button = Button("Quit", 40, False, True, BLACK, 525, 270, 150, 150, RED, DARK_RED)
 
 # Game related variables
 player = Player(400, 300)
@@ -358,36 +241,8 @@ while menu:
 
     # Basic Print
     win.fill(WHITE)
-    print_to_display(
-        win,
-        "Bubble Shooter",
-        "Times New Roman",
-        60,
-        True,
-        False,
-        DARK_BLUE_GRAY,
-        400,
-        100,
-        True,
-        False,
-        0,
-        0,
-    )
-    print_to_display(
-        win,
-        "By Arjun",
-        "Times New Roman",
-        30,
-        False,
-        True,
-        DARK_BLUE_GRAY,
-        400,
-        160,
-        True,
-        False,
-        0,
-        0,
-    )
+    print_to_display(win, "Bubble Shooter", "Times New Roman", 60, True, False, DARK_BLUE_GRAY, 400, 100, True, False, 0, 0)
+    print_to_display(win, "By Arjun", "Times New Roman", 30, False, True, DARK_BLUE_GRAY, 400, 160, True, False, 0, 0)
 
     # Buttons and functionality
     start_button.create(win)
@@ -426,13 +281,9 @@ while game:
         bubble.move()
 
     if bubble.offset == 0:
-        bubbles.append(
-            Bubble((random.randint(0, 89), random.randint(0, 37), 105))
-        )
+        bubbles.append(Bubble((random.randint(0, 89), random.randint(0, 37), 105)))
     if len(bubbles) == 0:
-        bubbles.append(
-            Bubble((random.randint(0, 89), random.randint(0, 37), 105))
-        )
+        bubbles.append(Bubble((random.randint(0, 89), random.randint(0, 37), 105)))
 
     if mouse_click[0] == 1 and shoot_timer == 0:
         bullets.append(Projectile(BROWN, 0.2, 10))
@@ -444,64 +295,27 @@ while game:
         bullet.move()
 
         for index, bubble in enumerate(bubbles):
-            if (
-                bubble.hitbox[0]
-                < bullet.x
-                < bubble.hitbox[0] + bubble.hitbox[2]
-                and bubble.hitbox[1]
-                < bullet.y
-                < bubble.hitbox[1] + bubble.hitbox[3]
-            ):
+            if bubble.hitbox[0] < bullet.x < bubble.hitbox[0] + bubble.hitbox[2] and bubble.hitbox[1] < bullet.y < bubble.hitbox[1] + bubble.hitbox[3]:
 
                 if bubble.radius > 20:
                     for i in range(5):
-                        eff_bubbles.append(
-                            Effect(
-                                bubble.x,
-                                bubble.y,
-                                0.5,
-                                bubble.radius - 15,
-                                bubble.color,
-                            )
-                        )
+                        eff_bubbles.append(Effect(bubble.x, bubble.y, 0.5, bubble.radius - 15, bubble.color))
 
                 else:
                     for i in range(5):
-                        eff_bubbles.append(
-                            Effect(
-                                bubble.x,
-                                bubble.y,
-                                0.5,
-                                bubble.radius - 8,
-                                bubble.color,
-                            )
-                        )
+                        eff_bubbles.append(Effect(bubble.x, bubble.y, 0.5, bubble.radius - 8, bubble.color))
 
                 bubbles.pop(index)
 
         for index, bullet in enumerate(bullets):
-            if (
-                bubble.hitbox[0]
-                < bullet.x
-                < bubble.hitbox[0] + bubble.hitbox[2]
-                and bubble.hitbox[1]
-                < bullet.y
-                < bubble.hitbox[1] + bubble.hitbox[3]
-            ):
+            if bubble.hitbox[0] < bullet.x < bubble.hitbox[0] + bubble.hitbox[2] and bubble.hitbox[1] < bullet.y < bubble.hitbox[1] + bubble.hitbox[3]:
                 bullets.pop(index)
 
     for eff_bubble in eff_bubbles:
         eff_bubble.bubble_pop_effect(win)
 
     # Game over check
-    if (
-        bubble.hitbox[0] + bubble.hitbox[2] >= player.hitbox_player[0]
-        and bubble.hitbox[0]
-        <= player.hitbox_player[0] + player.hitbox_player[2]
-        and bubble.hitbox[1] + bubble.hitbox[3] >= player.hitbox_player[1]
-        and bubble.hitbox[1]
-        <= player.hitbox_player[1] + player.hitbox_player[3]
-    ):
+    if bubble.hitbox[0] + bubble.hitbox[2] >= player.hitbox_player[0] and bubble.hitbox[0] <= player.hitbox_player[0] + player.hitbox_player[2] and bubble.hitbox[1] + bubble.hitbox[3] >= player.hitbox_player[1] and bubble.hitbox[1] <= player.hitbox_player[1] + player.hitbox_player[3]:
         print("hi")
 
     # Lag management
